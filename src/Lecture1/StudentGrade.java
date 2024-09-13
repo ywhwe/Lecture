2674fc4925b447e4a654e6f1f03e06dd3dd52686
ywhwe <esromer@kyonggi.ac.kr>
2024-09-13 10:23:36 +0900

    Testing print input values

--- src/Lecture1/StudentGrade.java
+++ src/Lecture1/StudentGrade.java
@@ -1,7 +1,39 @@
 package Lecture1;
 
+import java.util.*;
+
+class Students {
+    String studentInfo[];
+    
+    public void InputInfo(int num) {
+        Scanner scanner2 = new Scanner(System.in);
+        studentInfo = new String[num];
+        int count = 0;
+
+        do { 
+            studentInfo[count] = scanner2.nextLine();
+            count++;
+        } while (count < num);
+
+        for (int i = 0; i < studentInfo.length-1; i++) {
+            System.out.println(studentInfo[i]);
+        }
+
+        scanner2.close();
+    }
+}
+
 public class StudentGrade {
     public static void main(String[] args) {
-        System.out.print("hi");
+        StudentGrade main = new StudentGrade();
+        main.MyMain();
+    }
+
+    public void MyMain() {
+        Students students = new Students();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("학생수: ");
+        students.InputInfo(scanner.nextInt());
     }
 }
