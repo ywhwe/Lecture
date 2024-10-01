9d692ac0816e8807fc30894b11d1a3b93f44ce43
ywhwe <126853115+ywhwe@users.noreply.github.com>
2024-10-01 15:39:06 +0900

    Finished task
    1. Renamed all method 'write' to 'read'
    2. Method 'read'(which was 'write) in class 'User' has modified to get Store object for parameter
    3. From now, method 'findItem' can check user input pattern to identify merch code, index or keyword
    4. Changed methods; 'readAllItems', 'readAllUsers' to well functioning

--- src/Lecture3/Store.java
+++ src/Lecture3/Store.java
@@ -1,87 +1,146 @@
 package src.Lecture3;
 
 import java.util.*;
+import java.util.regex.*;
 import java.nio.charset.Charset;
 
 public class Store {
     public static void main(String[] args) {
         Store main = new Store();
+
         main.newMain();
     }
 
-    HashMap<Integer, Item> itemSet = new HashMap<>();
-    HashMap<String, User> userSet = new HashMap<>();
+    ArrayList<Item> itemList = new ArrayList<>();
+    ArrayList<User> userList = new ArrayList<>();
 
     void newMain() {
         Scanner scanner = new Scanner(System.in, Charset.forName("EUC-KR"));
 
-        String id;
-        Item item;
-        User user;
+        int select = -1;
 
-        int itemIndex = 1;
+        String key;
 
-        do {
-            id = scanner.next();
-            if (id.equals("0")) break;
-            item = new Item();
-            item.write(itemIndex, id, scanner);
-            itemSet.put(itemIndex, item);
-            itemIndex++;
-        } while(true);
+        readAllItems(scanner);
 
-        printAllItems();
+        readAllUsers(scanner);
 
-        do {
-            id = scanner.next();
-            if (id.equals("0")) break;
+        System.out.println();
 
-            user = new User();
+        while (select != 0) {
+            System.out.print("""
+            (1) 상품출력 (2) 사용자출력 (3) 상품검색 (4) 사용자검색
+            (5) 상품으로사용자검색 (6) 상품별판매현황 (7) 같이판매된상품
+            (0) 종료 =>\s""");
 
-            user.write(id, scanner);
+            select = scanner.nextInt();
 
-            while(scanner.nextInt() != 0) {
-                user.basket.add(itemSet.get(scanner.nextInt()));
+            switch (select) {
+                case 1: printAllItems();
+                        break;
+                case 2: printAllUsers();
+                        break;
+                case 3: System.out.print("상품검색 키워드: ");
+                        key = scanner.next();
+                        findItem(key);
+                        break;
+                case 4: System.out.print("사용자 검색 키워드: ");
+                        findUser(scanner.next());
+                        break;
             }
+        }
 
-            userSet.put(user.id, user);
-        } while (scanner.nextInt() != 0);
-
-        printAllUsers();
 
         scanner.close();
     }
 
-    void writeAllItems() {
+    void readAllItems(Scanner scanner) {
+        String id;
+        int itemIndex = 1;
+        Item item;
+
+        do {
+            id = scanner.next();
+            if (id.equals("0")) break;
+
+            item = new Item();
+
+            item.read(itemIndex, id, scanner);
+
+            itemList.add(item);
+
+            itemIndex++;
+        } while(true);
 
     }
 
-    void writeAllUsers() {
+    void readAllUsers(Scanner scanner) {
+        String id;
+        User user;
 
-    }
+        while (true) {
+            id = scanner.next();
 
-    void printAllItems() {
-        for (var value : itemSet.values()) {
-            System.out.printf("[%d] %s %s (%d원)\n",value.num, value.code, value.name, value.price);
+            if (id.equals("0")) break;
+
+            user = new User();
+
+            user.read(id, scanner, this);
+
+            userList.add(user);
         }
     }
 
+    void printAllItems() {
+        for (var item : itemList) {
+            item.print();
+        }
+
+        System.out.println();
+    }
+
     void printAllUsers() {
-        int sum, size;
+        for (var user : userList) {
+            user.print();
+        }
 
-        for (var value : userSet.values()) {
-            sum = 0;
-            size = value.basket.size();
+        System.out.println();
+    }
 
-            for (int i = 0; i < size; i++) {
-                sum += value.basket.get(i).price;
+    Item findItem(int index) {
+        for (var item : itemList) {
+            if (item.num == index) return item;
+        }
+        return null;
+    }
+
+    void findItem(String key) {
+        Pattern codePattern = Pattern.compile("^[A-Z]\\d{4}$");
+        Pattern indexPattern = Pattern.compile("^\\d+$");
+        Pattern kwdPattern = Pattern.compile("^[가-힣a-zA-Z0-9]+$");
+        int parsed;
+
+        if (codePattern.matcher(key).matches()) {
+            for (var item : itemList) {
+                if (item.matches(key)) item.print();
             }
-
-            System.out.printf("[%s] 총구매 %d건 %d원 (%d점)\n", value.id, size, sum, sum);
-
-            for (var item : value.basket) {
-                System.out.printf("%s ", item.name);
+        }
+        else if (indexPattern.matcher(key).matches()) {
+            parsed = Integer.parseInt(key);
+            for (var item : itemList) {
+                if (item.matches(parsed)) item.print();
             }
+        }
+        else if (kwdPattern.matcher(key).matches()) {
+            for (var item : itemList) {
+                if (item.contains(key)) item.print();
+            }
+        }
+    }
+
+    void findUser(String id) {
+        for (var user : userList) {
+            if (user.matches(id)) user.print();
         }
     }
 }
@@ -90,7 +149,7 @@ class Item {
     int num, price;
     String code, name;
 
-    void write(int index, String id, Scanner scan) {
+    void read(int index, String id, Scanner scan) {
         num = index;
         code = id;
         name = scan.next();
@@ -98,30 +157,60 @@ class Item {
     }
 
     void print() {
-
+        System.out.printf("[%d] %s %s (%d원)\n", num, code, name, price);
     }
 
-    void matches() {
+    boolean matches(String code) {
+        return code.equals(this.code);
+    }
 
+    boolean matches(int num) {
+        return num == this.num;
+    }
+
+    boolean contains(String key) {
+        return name.contains(key);
     }
 }
 
 class User {
     String id, pw;
+    int sum, point, tmp;
+
     ArrayList<Item> basket = new ArrayList<>();
 
-    void write(String uid, Scanner scan) {
-        id = uid;
-        pw = scan.next();
+    void read(String id, Scanner scan, Store store) {
 
-        scan.skip("0");
+        this.id = id;
+
+        if (id.equals("0")) return;
+
+        pw = scan.next();
+        point = scan.nextInt();
+
+        while(true) {
+            tmp = scan.nextInt();
+            if (tmp == 0) break;
+
+            basket.add(store.findItem(tmp));
+        }
+
+        for (var item: basket) {
+            sum += item.price;
+        }
     }
 
     void print() {
+        System.out.printf("[%s] 총구매 %d건 %d원 (%d점)\n", id, basket.size(), sum, point);
 
+        for (var item : basket) {
+                System.out.printf("%s ", item.name);
+            }
+
+        System.out.println();
     }
 
-    void matches() {
-
+    boolean matches(String id) {
+        return id.equals(this.id);
     }
 }
